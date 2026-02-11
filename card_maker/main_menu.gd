e617dd7f9281dfb8a143eeaf5d1fe63fb5c2fc63
benwayland-cell <ben.wayland2007@gmail.com>
2026-02-11 09:47:10 -0700

    Changed Main Menu's "+ Card Type" button to not be made with code.

--- card_maker/main_menu.gd
+++ card_maker/main_menu.gd
@@ -12,27 +12,21 @@ func open() -> void:
 		button.card_type = card_type
 		button.size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
 		button.connect("go_to_card_type", _on_button_pressed)
-		add_child(button)
-	
-	# set up the add card type button
-	var add_button := Button.new()
-	add_button.text = "+ Card Type"
-	add_button.size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
-	add_button.connect("pressed", _on_add_card_type)
-	add_child(add_button)
+		%CardTypeOptionsMenu.add_child(button)
 
 
 # cleans up the menu to be closed
 func close() -> void:
 	visible = false
 	
-	for child in get_children():
+	for child in %CardTypeOptionsMenu.get_children():
 		child.queue_free()
+
 
 func _on_button_pressed(card_type : CardType) -> void:
 	close()
 	%CardTypeMenu.open_card_type(card_type)
 
 
-func _on_add_card_type() -> void:
+func _on_card_type_button_pressed() -> void:
 	print("Add Card Type")
