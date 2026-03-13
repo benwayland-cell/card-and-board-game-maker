bf568feccc2ec75b77bd5fe9c5544ee4d6c2bd7e
benwayland-cell <ben.wayland2007@gmail.com>
2026-03-13 13:41:45 -0600

    Initial setup for a table editor

---
+++ table_editor/table_editor_UI/table_editor_ui.gd
@@ -0,0 +1,32 @@
+class_name TableEditorUI
+extends Control
+
+#signal add_node
+
+
+
+
+func _on_file_id_pressed(id: int) -> void:
+	match id:
+		0:
+			print("Save")
+		1:
+			print("Save As")
+		2:
+			print("Export")
+		3:
+			print("Import")
+
+
+func _on_insert_id_pressed(id: int) -> void:
+	match id:
+		0:
+			print("Insert Stack")
+		1:
+			print("Insert Single Snap")
+
+
+func _on_play_id_pressed(id: int) -> void:
+	match id:
+		0:
+			print("Play Game")
